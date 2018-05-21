50fd5111bd89078e5f68f6a716047c39b4ba1ae8
Victor Bolinches <vicboma1@users.noreply.github.com>
2018-05-22 00:38:56 +0200

    Add files via upload

--- KotlinNativeFramework/src/main/kotlin/native/KotlinNativeFramework.kt
+++ KotlinNativeFramework/src/main/kotlin/native/KotlinNativeFramework.kt
@@ -8,8 +8,43 @@
 
 import kotlinx.cinterop.*
 import platform.posix.*
+import platform.posix.errno_t
 
 class KotlinNativeFramework( ) {
     fun hi() = "Hello from Kotlin!"
-    fun fullName(f : String, s : String ) = "$s, $f"
+    fun fullName(f: String, s: String) = "$s, $f"
+
+    fun printAsync(str : String) = processor<String> { str }
+
+    private inline fun <reified T> processor(crossinline action : ()-> T ) {
+        val arena = Arena()
+        memScoped {
+            val thread = alloc<pthread_tVar>()
+            val rc = pthread_create(thread.ptr, null, staticCFunction {
+                _argC ->
+                initRuntimeIfNeeded()
+                val arg = konan.worker.attachObjectGraph<T>(stable = _argC)
+                println(arg)
+                null as COpaquePointer?
+            },konan.worker.detachObjectGraph { "pthread[${thread.rawPtr}] -> ${action()}" } )
+            pthread_join(thread.value, null)
+
+            if (rc != 0)
+                errExitEN(rc, "pthread_create")
+
+            pthread_join(thread.value, null)
+        }
+        arena.clear()
+    }
+
+    private fun errExitEN(en: Int, msg: String) {
+        do {
+            h_errno = en;
+            perror(msg);
+            exit(EXIT_FAILURE);
+        } while (true)
+    }
+
 }
+
+
